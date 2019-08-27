3da032141075444da55c5ae4c87499edd1209277
Jeremy Sintes <jeremy.sintes@gmail.com>
2019-08-27 16:37:04 +0200

    'update'

--- osmose-delegate-node/Update tuto EN.bat
+++ osmose-delegate-node/Update tuto EN.bat
@@ -1,4 +1,4 @@
-# Update tutorial :
+﻿# Update tutorial :
 # ------------------
 # Permet de convertir les tutos sous google docs en codelabs 
 # puis de les envoyer sur git via pull request
@@ -10,9 +10,9 @@ set TUTO_ID=1OC7XTDVL3yOAYyPX29Oa2K8PI8ibQgJRbD82YzObopg
 # read TUTO_ID
 
 echo "Extraction de contenu du googledocs"
-start claat export %TUTO_ID% && cd .. && git add * && git commit -m 'update' && git pull && git push
+claat export %TUTO_ID% && cd .. && git add * && git commit -m 'update' && git pull && git push
 
 echo "Codelab mis à jour et partagé"
 
-read -p "Appuyer sur une touche pour fermer ..."
-exit
+echo.
+pause
