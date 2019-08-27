cc42ba020c1b9f13a3420330e5a1bf2dfe48b35a
Jeremy Sintes <jeremy.sintes@gmail.com>
2019-08-27 16:42:44 +0200

    'update'

--- Update tuto FR.bat
+++ Update tuto FR.bat
@@ -10,7 +10,7 @@ echo "Lancement de la mise a jour du tuto"
 set TUTO_ID=1iJzcKuvbIe0GwTcHgo713VLS9VHyVraZl-SziWHwinE
 
 echo "Extraction de contenu du googledocs"
-claat export %TUTO_ID% && cd .. && git add * && git commit -m 'update' && git pull && git push
+claat export %TUTO_ID% && git add * && git commit -m 'update' && git pull && git push
 
 echo "Codelab mis a jour et push sur git"
 
