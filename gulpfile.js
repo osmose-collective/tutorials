be82b33e504345a82c84ed6a665cffadff751cbf
Jeremy Sintes <jeremy.sintes@gmail.com>
2019-09-09 17:15:01 +0200

    adding gh page

--- gulpfile.js
+++ gulpfile.js
@@ -940,3 +940,14 @@ gulp.task('publish:prod:views', (callback) => {
   const opts = { exclude: CODELABS_NAMESPACE, dry: DRY_RUN, deleteMissing: DELETE_MISSING };
   gcs.rsync(STAGING_BUCKET, PROD_BUCKET, opts, callback);
 });
+
+var gulp        = require('gulp');
+var deploy      = require('gulp-gh-pages');
+
+/**
+ * Push build to gh-pages
+ */
+gulp.task('deploy', function () {
+  return gulp.src("./dist/**/*")
+    .pipe(deploy())
+});
